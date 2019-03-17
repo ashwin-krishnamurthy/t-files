264aa360510ae7b8981693d6c787cfd10256aadb
Ashwin Krishnamurthy <ashwin.ganesh.03@gmail.com>
2019-03-17 12:19:14 -0700

    Create SyntaxError.py

---
+++ Lab 3.01/SyntaxError.py
@@ -0,0 +1,15 @@
+#Practice Importing Random
+import random
+random.randint(1, 6)
+random.randint(1, 6)
+print(random.randint(1, 6))
+print(random.randint(1, 6))
+print(random.randint(1, 6))
+
+#Look at the documentation of the random library
+import random
+print(random.random(1, 10)
+
+#Magic 8 Ball
+magic_ball = ['Outlook is good', 'Ask again later', 'Yes', 'no', 'Most likely no', 'most likely yes', 'Maybe', 'Outlook is not good']
+print (random.choice(magic_ball))
